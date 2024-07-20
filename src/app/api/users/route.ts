b94301fa934412e89bb47681e69fb2f20ceab723
Tyler Varzeas <tyler.varzeas@gmail.com>
2024-07-20 12:53:10 -0700

    RESET: No auth

--- src/app/api/users/route.ts
+++
@@ -1,8 +0,0 @@
-import { prisma } from '@/lib/prisma'
-
-export const dynamic = 'force-dynamic'
-
-export async function GET() {
-  const users = await prisma.user.findMany()
-  return Response.json(users, { status: 200 })
-}
--- src/app/error/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function ErrorPage() {
-  return (
-    <div className="page__container">
-      <p>Sorry, something went wrong</p>
-    </div>
-  )
-}
